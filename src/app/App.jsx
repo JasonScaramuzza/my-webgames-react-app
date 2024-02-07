629cf544c199a67a14beb3f83bb37a0df68e21bc
JasonScaramuzza <92160522+JasonScaramuzza@users.noreply.github.com>
2024-02-07 15:05:45 +0000

    Part 2 complete

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -8,10 +8,14 @@ function App() {
     <div className='main-page'>
         <section className='header'>
           <h1 className='title'>Jason Scaramuzza's Web Games</h1>
-          <p>Welcome to my webpage. Here, you can play about with some games I've made in React. Enjoy!</p>
+          <p className="intro">Welcome to my webpage. Here, you can play about 
+            with some games I've made in React. Enjoy!</p>
         </section>
-        <section className='page'>      
-        <Link to={'/sudoku'}> Click here to play sudoku!</Link>
+        <section className='page'>
+          <h2 className="h2-style-1">Personal Projects</h2>   
+          <Link to={'/sudoku'}> Click here to play sudoku!</Link>
+          <h2 className="h2-style-1">Online Courses Exercises</h2>
+          <Link to={'/portfolioProject'}> FrontendMasters 'Getting Started with CSS' - Portfolio project</Link>
         </section>
     </div>
   );
