9e6cabcbc3399c346d3c138279bb3bdfe15c868a
JasonScaramuzza <92160522+JasonScaramuzza@users.noreply.github.com>
2024-02-10 16:38:54 +0000

    Portfolio home page changed
    Work in progress. 1 Hour through the course on FronendMasters.

--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from './app/App';
 import SudokuPage from './sudoku/SudokuPage'
-import MyHTML from "./portfolioProject/MyHTML";
 
 
 export const MyRoutes = () => {
@@ -11,7 +10,6 @@ export const MyRoutes = () => {
                 <Route path="/" element={<App />}/> 
                 <Route path="/home" element={<App />}/>
                 <Route path="/sudoku" element={<SudokuPage />} />
-                <Route path="/portfolioProject" element={<MyHTML />} />
             </Routes>
         </Router>
     )
--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,22 +1,62 @@
 import { Link } from 'react-router-dom';
 import './App.css';
 
+const NAVBAR = ["Jason Scaramuzza","Projects","About","Contact","LinkedIn","Indeed Resume","Github"];
+
 //MAIN PAGE
 
 function App() {
   return (
-    <div className='main-page'>
-        <section className='header'>
-          <h1 className='title'>Jason Scaramuzza's Web Games</h1>
-          <p className="intro">Welcome to my webpage. Here, you can play about 
-            with some games I've made in React. Enjoy!</p>
-        </section>
-        <section className='page'>
-          <h2 className="h2-style-1">Personal Projects</h2>   
-          <Link to={'/sudoku'}> Click here to play sudoku!</Link>
-          <h2 className="h2-style-1">Online Courses Exercises</h2>
-          <Link to={'/portfolioProject'}> FrontendMasters 'Getting Started with CSS' - Portfolio project</Link>
-        </section>
+    <div className="main-page">
+      <section id="nav-bar">
+        <nav>
+          <ul>
+            <li>
+              <h1><a href="home">{NAVBAR[0]}</a></h1>
+            </li>
+            <li><a href="#projects">{NAVBAR[1]}</a></li> 
+            <li><a href="about">{NAVBAR[2]}</a></li>
+            <li><a href="#contact-me">{NAVBAR[3]}</a></li>
+            <li>
+              <a 
+                href="https://www.linkedin.com/in/jason-scaramuzza-842ba8181/" 
+                target="_blank">
+                {NAVBAR[4]}
+              </a>
+            </li>
+            <li>
+              <a 
+                href="https://profile.indeed.com/p/jasons-vd2jcl7"
+                target="_blank">
+                {NAVBAR[5]}
+              </a>
+            </li>
+            <li>
+              <a 
+                href="https://github.com/JasonScaramuzza"
+                target="_blank">
+                {NAVBAR[6]}
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </section>
+
+      <section id="intro">
+        <p className="name">Hi my name is <span>Jason Scaramuzza.</span></p>
+        <h2>I'm a software engineer.</h2>
+        <p> I specialize in frontend development with experience in HTML, 
+            CSS, JavaScript and React </p>
+        <p>Currently I am engaged in work expereince at a Risk Management SaaS 
+            organisation Riskwell whilst looking for a job.</p>
+      </section>
+
+      <section id="contact-me">
+        <h2>Contact me</h2>
+        <p>I'm always interested in hearing about new job opportunities, 
+          learning new languages and new design techniques.</p>
+        <p><a href="mailto:jason.scaramuzza@gmail.com">Email me</a></p>
+      </section>
     </div>
   );
 }
