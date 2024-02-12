b5b56cb67249a9611f6ca7aac4da5ff07c0699bd
JasonScaramuzza <92160522+JasonScaramuzza@users.noreply.github.com>
2024-02-12 12:43:43 +0000

    Front page structure complete
    front page structure complete.
    Next TODO:
    -Add Sudoku project in main page projects section.
    -Fix Sudoku CSS and HTML.
    -Add Project's page where 'RecyclerView' kind of style where lists all projects.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { faCubes } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
@@ -30,7 +30,7 @@ function App() {
             <li>
               <a 
                 href="https://www.linkedin.com/in/jason-scaramuzza-842ba8181/" 
-                target="_blank">
+                target="_blank" rel="noreferrer">
                 <FontAwesomeIcon icon={faLinkedin} aria-hidden="true"/> 
                 <span class="sr-only">{NAVBAR[4]}</span>
               </a>
@@ -38,7 +38,7 @@ function App() {
             <li>
               <a 
                 href="https://github.com/JasonScaramuzza"
-                target="_blank">
+                target="_blank" rel="noreferrer">
                 <FontAwesomeIcon icon={faGithub} aria-hidden="true"/>
                 <span class="sr-only">{NAVBAR[5]}</span>
               </a>
@@ -46,29 +46,156 @@ function App() {
             <li>
               <a 
                 href="https://profile.indeed.com/p/jasons-vd2jcl7"
-                target="_blank">
+                target="_blank" rel="noreferrer"
+                class="link-style-button">
                 {NAVBAR[6]}
               </a>
             </li>
           </ul>
         </nav>
       </section>
+      <div class="gradient-1"></div>
 
       <section id="intro">
         <p className="name">Hi my name is <span>Jason Scaramuzza.</span></p>
         <h2>I'm a software engineer.</h2>
         <p> I specialize in frontend development with experience in HTML, 
             CSS, JavaScript and React </p>
-        <p>Currently I am engaged in work expereince at a Risk Management SaaS 
-            organisation Riskwell whilst looking for a job.</p>
+        <p>Currently I am engaged in work expereince at Riskwell, 
+        a Risk Management SaaS based organisation, whilst looking for a job.</p>
       </section>
+      <div class="gradient-1"></div>
 
-      <section id="contact-me">
-        <h2>Contact me</h2>
-        <p>I'm always interested in hearing about new job opportunities, 
-          learning new languages and new design techniques.</p>
-        <p><a href="mailto:jason.scaramuzza@gmail.com">Email me</a></p>
-      </section>
+      <div class="section-projects-container">
+        <section id="projects">
+          <h2>Projects I'm proud of</h2>
+          <article>
+            <div class="project-text">
+              <h4>Latest Project</h4>
+              <h3>Example Project Name</h3>
+              <p>Description of the project. This should be fairly concise while 
+                also describing the key components that you developed or worked on. 
+                It can be as long as you need it to be but should at least be a few 
+                sentences long. Be sure to include specific links anywhere in the 
+                description. A link looks like <a 
+                href="https://frontendmasters.github.io/grid-flexbox-v2/">this</a>, 
+                and multiple links look <a href="#">like this</a> and <a href="#">like this</a>.
+              </p>
+              <h4>Technologies used include:</h4>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>SVG</li>
+              </ul>
+            </div>
+            <div class="project-img">
+              <img 
+                src="https://play-lh.googleusercontent.com/1-hPxafOxdYpYZEOKzNIkSP43HXCNftVJVttoo4ucl7rsMASXW3Xr6GlXURCubE1tA=w3840-h2160-rw" 
+                alt="placeholder">
+              </img>
+            </div>
+          </article>
+          <article>
+            <div class="project-text">
+              <h4>Placeholder Project Type</h4>
+              <h3>Example Project Name</h3>
+              <p>Description of the project. This should be fairly concise while 
+                also describing the key components that you developed or worked on. 
+                It can be as long as you need it to be but should at least be a few 
+                sentences long. Be sure to include specific links anywhere in the 
+                description. A link looks like <a 
+                href="https://frontendmasters.github.io/grid-flexbox-v2/">this</a>, 
+                and multiple links look <a href="#">like this</a> and <a href="#">like this</a>.
+              </p>
+              <h4>Technologies used include:</h4>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>SVG</li>
+              </ul>
+            </div>
+            <div class="project-img">
+              <img 
+                src="https://play-lh.googleusercontent.com/1-hPxafOxdYpYZEOKzNIkSP43HXCNftVJVttoo4ucl7rsMASXW3Xr6GlXURCubE1tA=w3840-h2160-rw" 
+                alt="placeholder">
+              </img>
+            </div>
+          </article>
+          <article>
+            <div class="project-text">
+              <h4>Placeholder Project Type</h4>
+              <h3>Example Project Name</h3>
+              <p>Description of the project. This should be fairly concise while 
+                also describing the key components that you developed or worked on. 
+                It can be as long as you need it to be but should at least be a few 
+                sentences long. Be sure to include specific links anywhere in the 
+                description. A link looks like <a 
+                href="https://frontendmasters.github.io/grid-flexbox-v2/">this</a>, 
+                and multiple links look <a href="#">like this</a> and <a href="#">like this</a>.
+              </p>
+              <h4>Technologies used include:</h4>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>SVG</li>
+              </ul>
+            </div>
+            <div class="project-img">
+              <img 
+                src="https://play-lh.googleusercontent.com/1-hPxafOxdYpYZEOKzNIkSP43HXCNftVJVttoo4ucl7rsMASXW3Xr6GlXURCubE1tA=w3840-h2160-rw" 
+                alt="placeholder">
+              </img>
+            </div>
+          </article>
+        </section>
+      </div>
+
+      <div class="gradient-1"></div>
+      <div class="section-contact-me-container">
+        <section id="contact-me">
+          <h2>Contact me</h2>
+          <p>I'm always interested in hearing about new job opportunities, 
+            learning new languages and new design techniques.</p>
+          <p>
+            <a 
+              href="mailto:jason.scaramuzza@gmail.com" 
+              class="link-style-button">
+              Email me
+            </a>
+          </p>
+        </section>
+      </div>
+      <div class="gradient-1"></div>
+
+      <footer>
+        <h2>Jason Scaramuzza &middot; Software Engineer</h2>
+        <ul>
+          <li>
+            <a 
+              href="https://www.linkedin.com/in/jason-scaramuzza-842ba8181/" 
+              target="_blank" rel="noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} aria-hidden="true"/> 
+              <span class="sr-only">{NAVBAR[4]}</span>
+            </a>
+          </li>
+          <li>
+            <a 
+              href="https://github.com/JasonScaramuzza"
+              target="_blank" rel="noreferrer">
+              <FontAwesomeIcon icon={faGithub} aria-hidden="true"/>
+              <span class="sr-only">{NAVBAR[5]}</span>
+            </a>
+          </li>
+          <li>
+            <a 
+              href="mailto:jason.scaramuzza@gmail.com">
+              <FontAwesomeIcon icon={faEnvelope} aria-hidden="true"/>
+              <span class="sr-only">{NAVBAR[3]}</span>
+            </a>
+          </li>
+        </ul>
+        <p><small>&copy; 2024 Jason Scaramuzza. All rights reserved.</small></p>
+      </footer>
     </div>
   );
 }
