e19d27c2ccbfa1edefbd3d588ba26bdee40c38d6
JasonScaramuzza <92160522+JasonScaramuzza@users.noreply.github.com>
2024-02-13 19:43:38 +0000

    Split footer and contact me section into components

---
+++ src/globalComponents/myContactMe/MyContactMe.jsx
@@ -0,0 +1,22 @@
+import './MyContactMe.css'
+
+const MyContactMe = () =>  {
+    return(
+        <div className="section-contact-me-container">
+            <section id="contact-me">
+                <h2>Contact me</h2>
+                <p>I'm always interested in hearing about new opportunities, 
+                learning new languages and design techniques.</p>
+                <p>
+                <a 
+                    href="mailto:jason.scaramuzza@gmail.com" 
+                    className="link-style-button">
+                    Email me
+                </a>
+                </p>
+            </section>
+        </div>
+    );
+}
+
+export default MyContactMe;
--- src/pageMain/App.jsx
+++ src/pageMain/App.jsx
@@ -1,9 +1,8 @@
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 import MyHeader from '../globalComponents/myHeader/MyHeader';
+import MyContactMe from '../globalComponents/myContactMe/MyContactMe';
+import MyFooter from '../globalComponents/myFooter/MyFooter';
 
 import './App.css';
 
@@ -114,52 +113,12 @@ function App() {
 
       <div className="gradient-1"></div>
 
-      <div className="section-contact-me-container">
-        <section id="contact-me">
-          <h2>Contact me</h2>
-          <p>I'm always interested in hearing about new opportunities, 
-            learning new languages and design techniques.</p>
-          <p>
-            <a 
-              href="mailto:jason.scaramuzza@gmail.com" 
-              className="link-style-button">
-              Email me
-            </a>
-          </p>
-        </section>
-      </div>
-      
+      <MyContactMe />
+
       <div className="gradient-1"></div>
 
-      <footer>
-        <h2>Jason Scaramuzza &middot; Software Engineer</h2>
-        <ul>
-          <li>
-            <a 
-              href="https://www.linkedin.com/in/jason-scaramuzza-842ba8181/" 
-              target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faLinkedin} aria-hidden="true"/> 
-              <span className="sr-only">LinkedIn</span>
-            </a>
-          </li>
-          <li>
-            <a 
-              href="https://github.com/JasonScaramuzza"
-              target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faGithub} aria-hidden="true"/>
-              <span className="sr-only">Github</span>
-            </a>
-          </li>
-          <li>
-            <a 
-              href="mailto:jason.scaramuzza@gmail.com">
-              <FontAwesomeIcon icon={faEnvelope} aria-hidden="true"/>
-              <span className="sr-only">Contact</span>
-            </a>
-          </li>
-        </ul>
-        <p><small>&copy; 2023 Jason Scaramuzza. All rights reserved.</small></p>
-      </footer>
+      <MyFooter />
+
     </div>
   );
 }
