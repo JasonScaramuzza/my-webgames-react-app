580a4d1fdcc0df3a9df00c1be1456960b7274bc7
JasonScaramuzza <92160522+JasonScaramuzza@users.noreply.github.com>
2024-02-13 20:23:13 +0000

    About Page Started

--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from './pageMain/App';
-import SudokuPage from './projects/sudoku/SudokuPage'
+import AboutPage from './pageAbout/AboutPage';
+import SudokuPage from './projects/sudoku/SudokuPage';
 
 
 export const MyRoutes = () => {
@@ -9,6 +10,7 @@ export const MyRoutes = () => {
             <Routes>
                 <Route path="/" element={<App />}/> 
                 <Route path="/home" element={<App />}/> 
+                <Route path="/about" element={<AboutPage />}/> 
                 <Route path="/sudoku" element={<SudokuPage />} />
             </Routes>
         </Router>
---
+++ src/pageAbout/AboutPage.jsx
@@ -0,0 +1,38 @@
+import { Link } from 'react-router-dom';
+
+import MyHeader from '../globalComponents/myHeader/MyHeader';
+import MyContactMe from '../globalComponents/myContactMe/MyContactMe';
+import MyFooter from '../globalComponents/myFooter/MyFooter';
+
+import './AboutPage.css';
+
+//MAIN PAGE
+
+function AboutPage() {
+  return (
+    <div className="about-page"> 
+
+        <MyHeader />
+
+        <div className="gradient-1"></div>
+
+        <section id="page-content">
+          <h2>About Jason Scaramuzza</h2>
+          <article>
+
+          </article>
+        </section>
+
+        <div className="gradient-1"></div>
+
+        <MyContactMe />
+
+        <div className="gradient-1"></div>
+
+        <MyFooter />
+
+    </div>
+  );
+}
+
+export default AboutPage;
