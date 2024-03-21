8ede93d9352a906392e408e15f2af8773f796770
JasonScaramuzza <92160522+JasonScaramuzza@users.noreply.github.com>
2024-03-21 15:11:55 +0000

    Calendar Week View Static design sorted

--- src/projects/calendarDateSelector/MyCalendarWeekView.jsx
+++ src/projects/calendarDateSelector/MyCalendarWeekView.jsx
@@ -1,20 +1,28 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 import CalendarCell from "./CalendarCell";
 import "./MyCalendarWeekView.css";
 
 const MyCalendarWeekView = (props) => {
   const locales = props.locales;
   const events = props.events;
-  console.log(locales, events);
+  const [daysInMonth, setDaysInMonth] = useState(
+    Array.from({ length: 5 }, () => Array(7).fill()),
+  );
+  //console.log(locales, events);
 
   return (
     <div className="calendar-container">
       <div className="month-header">
+        <CalendarCell header={""} />
         <CalendarCell header={"<<"} />
         <CalendarCell header={"<"} />
         <CalendarCell className="month-selector" header={"August 2022"} />
         <CalendarCell header={">"} />
         <CalendarCell header={">>"} />
+        <CalendarCell header={""} />
       </div>
+
       <div className="day-of-week-container">
         <div className="day-of-week">Monday</div>
         <div className="day-of-week">Tuesday</div>
@@ -24,7 +32,21 @@ const MyCalendarWeekView = (props) => {
         <div className="day-of-week">Saturday</div>
         <div className="day-of-week">Sunday</div>
       </div>
-      <div className="calendar-cells-container"></div>
+
+      <div className="calendar-cells-container">
+        {daysInMonth?.map((week, rowIndex) => {
+          return week.map((day, columnIndex) => {
+            return (
+              <div
+                className="calendar-cell"
+                key={"dIM" + rowIndex + columnIndex}
+              >
+                <CalendarCell header={day} />
+              </div>
+            );
+          });
+        })}
+      </div>
     </div>
   );
 };
